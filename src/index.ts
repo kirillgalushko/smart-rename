8a44d57adb795c2524131db64c7df112e421b654
Kirill Galushko <yoursgk@mail.ru>
2024-12-02 21:40:12 +0600

    Add logs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import { ensureDir } from 'fs-extra';
 import path from 'path';
+import log from 'log';
 
 export const RESULT_DIR = 'rename_result';
 
 export const createResultDirectory = async (inputPath: string) => {
   const resultDirPath = path.join(inputPath, RESULT_DIR);
-  await ensureDir(resultDirPath);
+  log.info(`Attempting to create result directory: ${resultDirPath}`);
+
+  try {
+    await ensureDir(resultDirPath);
+    log.info(`Result directory created successfully: ${resultDirPath}`);
+  } catch (error) {
+    log.error(`Error creating result directory: ${resultDirPath}`, error);
+    throw error;
+  }
 };
 
 export const rename = async (inputPath: string) => {
-  await createResultDirectory(inputPath);
+  log.info(`Starting the renaming process for inputPath: ${inputPath}`);
+
+  try {
+    await createResultDirectory(inputPath);
+    log.info('Renaming process completed successfully');
+  } catch (error) {
+    log.error('Renaming process failed', error);
+  }
 };
--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+  },
+  resolve: {
+    alias: {
+      log: path.resolve(__dirname, 'src/log.ts'),
+    },
   },
 });
